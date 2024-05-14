c51deee70087de9f465f511638e8c713f85cc283
M23W7356 <166461376+M23W7356@users.noreply.github.com>
2024-05-14 15:32:55 +0900

    Update homework4.py

--- 2024_spring/lec04/homework4.py
+++ 2024_spring/lec04/homework4.py
@@ -4,11 +4,13 @@ def list_to_dict(input_list):
     This function should return a dictionary in which each element of 
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
+    
+    here is some extra text.
     '''
-    mydict ={}
-    for k,v in enumerate(input_list):
-        mydict[k] =v
-    return(mydict)
+    output_dict = {}
+    for n,item in enumerate input_list:
+        output_dict[n] = item
+    return output_dict
 
 
 
